b35e985592a7f1f6e9612ebf1e7e25258b1a8e5f
Michael Shyu <michaelshyu11@gmail.com>
2023-11-06 21:59:42 -0500

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions in this file\
+
+function writeCards(arr, event) {
+    const newArray = [];
+    let newString;
+    for (let i = 0; i < arr.length; i++) {
+        newString = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+        newArray.push(newString);
+    }
+
+    return newArray;
+}
+
+function countDown(pos) {
+    while(pos >= 0) {
+        console.log(pos);
+        pos--;
+    }
+}
